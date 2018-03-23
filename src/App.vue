65f4491e009d20aec9d43b4939708b9c5d9ceae1
shoestrong <zqc_2007@yeah.net>
2018-03-23 20:26:44 +0800

    echarts插件

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,61 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <Chart id="demo" :option="option"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Chart from './components/Chart'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      option: {
+        title: {
+          text: "vue_echarts"
+        },
+        tooltip: {},
+        legend: {
+            data:['销量']
+        },
+        xAxis: {
+              data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+          },
+          yAxis: {},
+          series: [{
+              name: '销量',
+              type: 'bar',
+              data: [5, 20, 36, 10, 10, 20]
+          }]
+      },
+      chartData: {
+        xData: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+        sData: [5, 20, 36, 10, 10, 70]
+      }
+    };
+  },
   components: {
-    HelloWorld
+    Chart
+  },
+  mounted() {
+    this.refreshData();
+  },
+  methods: {
+    //添加refreshData方法进行自动设置数据
+    refreshData() {
+      //横轴数据
+      let xData = this.chartData.xData,
+        //系列值
+          sData = this.chartData.sData;
+      for (let i = 0; i < xData.length; i++) {
+        //此处使用let是关键，也可以使用闭包。原理不再赘述
+           setTimeout(() => {
+          this.option.xAxis.data.push(xData[i]);
+          this.option.series[0].data.push(sData[i]);
+        }, 1000*i)//此处要理解为什么是1000*i
+      }
+    }
   }
 }
 </script>
@@ -26,3 +70,5 @@ export default {
   margin-top: 60px;
 }
 </style>
+
+
--- src/main.js
+++ src/main.js
@@ -2,12 +2,15 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import echarts from 'echarts'
+
+Vue.prototype.$echarts = echarts
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    components: { App },
+    template: '<App/>'
 })
